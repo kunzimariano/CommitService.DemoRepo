e563f8fd919b2699ad8bb76615122ab639e11198
jkoberg <joe.koberg@versionone.com>
2014-01-24 18:23:31 -0500

    Very syntax

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -1,6 +1,6 @@
 namespace KGzznnn
 {
-    public static int SLEEP_FACTOR = 1000000.0
+    public static int SLEEP_FACTOR = 1000000.0;
     public class iNeed
     {
         Threading.Thread.Sleep(999 * SLEEP_FACTOR);
