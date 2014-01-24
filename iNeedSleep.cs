5739f5ec8c363dd31e110c5a0291664a1544c4ea
jkoberg <joe.koberg@versionone.com>
2014-01-24 18:24:46 -0500

    So member

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -1,8 +1,10 @@
 namespace KGzznnn
 {
-    public static int SLEEP_FACTOR = 1000000.0;
     public class iNeed
     {
-        Threading.Thread.Sleep(999 * SLEEP_FACTOR);
+        public static int SLEEP_FACTOR = 1000000.0;
+        public void Main() {
+            Threading.Thread.Sleep(999 * SLEEP_FACTOR);
+        }
     }
 }
