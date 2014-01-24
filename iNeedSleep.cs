17c6fe4008882ff1daa5b0c2e1c2447737f53bde
jkoberg <joe.koberg@versionone.com>
2014-01-24 18:23:21 -0500

    Much sleep

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -1,8 +1,8 @@
 namespace KGzznnn
 {
+    public static int SLEEP_FACTOR = 1000000.0
     public class iNeed
     {
-        
-        Threading.Thread.Sleep(10221);
+        Threading.Thread.Sleep(999 * SLEEP_FACTOR);
     }
 }
