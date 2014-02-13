0e1c7453c686bfb7c9e015f07f5a925d8fbab64a
Josh Gough <jsgough@gmail.com>
2014-02-13 11:17:29 -0500

    Update iNeedSleep.cs

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -2,7 +2,7 @@ namespace KGzznnn
 {
     public class iNeed
     {
-        public static int SLEEP_FACTOR = 10220.0;
+        public static int SLEEP_FACTOR = 10244420.0;
         public void Main() {
             Threading.Thread.Sleep(999 * SLEEP_FACTOR + 11);
         }
