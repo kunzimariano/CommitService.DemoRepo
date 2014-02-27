78364502db655d71da808f62dc5ebbfdf0acbf7c
Josh Gough <jsgough@gmail.com>
2014-02-27 10:45:25 -0500

    Update iNeedSleep.cs
    Can haz azure?

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -2,7 +2,7 @@ namespace KGzznnn
 {
     public class iNeed
     {
-        public static int SLEEP_FACTOR = 8838383.0;
+        public static int SLEEP_FACTOR = 883332342.0;
         public void Main() {
             Threading.Thread.Sleep(999 * SLEEP_FACTOR + 11);
         }
