a1463f61cf1f3e32f07197ff1242650cc127db33
kunzimariano <kunzi.mariano@gmail.com>
2014-09-18 12:04:40 -0300

    S-01001 you also need quality for your sleeping.

--- iNeedSleep.cs
+++ iNeedSleep.cs
@@ -4,6 +4,7 @@ namespace KGzznnn
     {
         public static int SLEEP_FACTOR = 08013.0;
         public void Main() {
+            // just sleeping is not enough, you also need quality
             Threading.Thread.Sleep(999 * SLEEP_FACTOR + 11);
         }
     }
